6640a17147076da3fb977d1231957097043b9f3f
admin <admin@admins-MacBook-Air.local>
2025-01-04 10:54:53 +0500

    add img

---
+++ src/components/form/Form.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Form.css";
+import city_img from "../../imgs/city_img.png";
+
+const Form = () => {
+  return (
+    <div className="form_page">
+      <div id="container"></div>
+    </div>
+  );
+};
+
+export default Form;
