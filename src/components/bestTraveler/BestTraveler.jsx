6a1fbdebcdf6fa6054ddc2e41507f567feaa62e5
admin <admin@admins-MacBook-Air.local>
2025-01-04 17:29:27 +0500

    add animation page update

--- src/components/bestTraveler/BestTraveler.jsx
+++ src/components/bestTraveler/BestTraveler.jsx
@@ -1,34 +1,3 @@
-// import React from "react";
-// import "./BestTraveler.css";
-// import BestTravelerImg from "../../imgs/Best Traveler’s.png";
-// import { bestInfo } from "../../static/bestInfo";
-
-// function BestTraveler() {
-//   return (
-//     <div className="best_traveler_page">
-//       <div id="container" className="best_traveler_container">
-//         <img src={BestTravelerImg} alt="best traveler img text" />
-//         <h1>Shared photos</h1>
-//         <span>
-//           Lorem Ipsum is simply dummy text of the printing and typesetting
-//           industry. Lorem Ipsum has been the industry's standard dummy text ever
-//           since the 1500s,
-//         </span>
-
-//         <div className="best_traveler_card_container">
-//           {bestInfo?.map((item, inx) => (
-//             <div key={inx} className="best_traveler_card">
-//               <img src={item?.img} alt="best traveler" />
-//             </div>
-//           ))}
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default BestTraveler;
-
 import React from "react";
 import "./BestTraveler.css";
 import BestTravelerImg from "../../imgs/Best Traveler’s.png";
